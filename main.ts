87dc0950f9b69590d06d7d19e728ea8ccd8e675a
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-26 13:58:44 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-basic.showNumber(0)
+input.onButtonPressed(Button.A, function () {
+    for (let y = 0; y <= 4; y++) {
+        led.plot(y, 4 - y)
+        basic.pause(500)
+    }
+})
 basic.forever(function () {
 	
 })
